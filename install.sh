087f868bcb0aed5515c5b37ccea114c3910904f3
Chris Laffra <laffra@uber.com>
2018-11-05 11:11:11 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 # Project dependencies:
 
-pip install AppKit
-
-exit
-
+brew install pkg-config libffi
+export PKG_CONFIG_PATH=/usr/local/Cellar/libffi/3.2.1/lib/pkgconfig/:$PKG_CONFIG_PATH
+brew install gobject-introspection
+brew install cairo
 pip install pycairo
-pip install Quartz
+pip install pyobjc-core
+pip install pyobjc-framework-Quartz
+pip install AppKit
 pip install py2app
 pip install rumps
 pip install psutil

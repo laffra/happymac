35441cb681231ae60f49e4d95c4aec28f04f00db
Chris Laffra <laffra@uber.com>
2018-11-10 14:50:52 +0100

    Add app disambiguation by grabbing top left window pixel

--- src/versions/v00001/utils.py
+++ src/versions/v00001/utils.py
@@ -1,10 +1,14 @@
 #pylint: disable=E1101
+#pylint: disable=E0611
 
 import AppKit
 import log
 import os
 import process
+import psutil
 import Quartz
+from Quartz import CG, CoreGraphics
+import struct
 import time
 import threading
 import traceback
@@ -30,6 +34,19 @@ def get_active_chrome_tabs():
 def get_active_window_name():
     return get_window_name(get_current_app_pid())
 
+def get_active_window_dimensions():
+    return get_window_dimensions(get_current_app_pid())
+
+def get_screen_pixel(x, y):
+    image = CG.CGWindowListCreateImage(
+        CoreGraphics.CGRectMake(x, y, 2, 2),
+        CG.kCGWindowListOptionOnScreenOnly,
+        CG.kCGNullWindowID,
+        CG.kCGWindowImageDefault)
+    bytes = CG.CGDataProviderCopyData(CG.CGImageGetDataProvider(image))
+    b, g, r, a = struct.unpack_from("BBBB", bytes, offset=0)
+    return (r, g, b, a)
+
 def is_chrome_window(window):
     return is_active_window(window) and window.valueForKey_('kCGWindowOwnerName') == "Google Chrome"
 
@@ -42,6 +59,13 @@ def get_window_name(pid):
     windows = [window for window in get_all_windows() if is_active_window(window, pid)]
     return windows and windows[0].get('kCGWindowName', '') or ''
 
+def get_window_dimensions(pid):
+    windows = [window for window in get_all_windows() if is_active_window(window, pid)]
+    if windows:
+        bounds = windows[0].get('kCGWindowBounds')
+        return (bounds['X'], bounds['Y'], bounds['Width'], bounds['Height'])
+    return (0,0,0,0)
+
 def clear_windows_cache():
     global all_windows
     all_windows = None
@@ -50,6 +74,9 @@ def get_all_windows():
     global all_windows
     if not all_windows:
         all_windows = Quartz.CGWindowListCopyWindowInfo(Quartz.kCGWindowListExcludeDesktopElements, Quartz.kCGNullWindowID)
+    for window in all_windows:
+        if False and window.valueForKey_('kCGWindowIsOnscreen') :
+            print window
     return all_windows
 
 def run_osa_script(script):
@@ -66,6 +93,7 @@ class Timer(threading.Thread):
             time.sleep(self.interval)
             try:
                 self.callback()
+            except psutil.NoSuchProcess:
+                pass # this is normal
             except Exception as e:
-
-                log.log("Error in Timer callback %s: %s" % (self.callback.im_func.__name__, e))
+                log.log("Error in Timer callback '%s': %s" % (self.callback.im_func.__name__, e))

0e236d74ef47883fa3acc8214efea2d64aee702e
Chris Laffra <laffra@uber.com>
2018-11-10 14:48:36 +0100

    Add a pie chart to the report

--- src/versions/v00001/activity.py
+++ src/versions/v00001/activity.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import error
 import log
@@ -14,9 +15,18 @@ INIT_QUERY = """CREATE TABLE IF NOT EXISTS activities (
     timestamp float,
     system float,
     cpu float,
-    name text NOT NULL,
-    title text NOT NULL
+    x number,
+    y number,
+    width number,
+    height number,
+    pixel text,
+    location text,
+    pid text,
+    ppid text,
+    name text,
+    title text
 )"""
+
 
 home_dir = os.path.join(os.path.expanduser("~"), "HappyMacApp")
 
@@ -38,10 +48,19 @@ def update():
         return
     connection = sqlite3.connect(get_activity_path())
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO activities (timestamp,system,cpu,name,title) VALUES(?,?,?,?,?)", [
+    x, y, w, h = utils.get_active_window_dimensions()
+    cursor.execute("INSERT INTO activities (timestamp,system,cpu,x,y,width,height,pixel,location,pid,ppid,name,title) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)", [
         int(time.time()),
         process.cpu(-1),
         cpu,
+        x,
+        y,
+        w,
+        h,
+        repr(utils.get_screen_pixel(x + 4, y + 4)),
+        process.location(pid),
+        pid,
+        process.parent_pid(pid),
         utils.get_current_app_name(),
         utils.get_active_window_name()
     ])
@@ -55,29 +74,124 @@ def get_activities():
 def generate_report():
     try:
         filename = get_report_path()
-        log.log("Generate report %s" % filename)
+        activities = get_activities()
+        log.log("Generate report with %d event in file %s" % (len(activities), filename))
         with open(filename, "w") as output:
-            output.write("<table border=1>")
-            output.write("<tr><th>When</th><th>CPU</th><th>APP CPU</th><th>APP NAME</th><th>Window/Tab Title</td></tr>")
-            for timestamp, cpu, app_cpu, app_name, window_title in get_activities():
-                output.write("""
-                <tr>
-                <td>%s</td>
-                <td>%d%%</td>
-                <td>%d%%</td>
-                <td>%s</td>
-                <td>%s</td>
-                </tr>
-                """ % (
-                    datetime.datetime.fromtimestamp(timestamp),
-                    int(cpu * 100),
-                    int(app_cpu * 100),
-                    app_name.encode('ascii','ignore'),
-                    window_title.encode('ascii','ignore'),
-                ))
-            output.write("</table>")
+            generate_header(output)
+            generate_pie_chart(output, activities)
+            generate_full_table(output, activities)
+            generate_footer(output)
         webbrowser.open("file://%s" % filename)
     except:
         error.error("Cannot generate report")
+
+def generate_header(output):
+    output.write("""
+        <html>
+            <head>
+                <title>HappyMac Report</title>
+                <script src="https://www.chartjs.org/dist/2.7.3/Chart.bundle.js"></script>
+                <script src="https://www.chartjs.org/samples/latest/utils.js"></script>
+            </head>
+            <body>
+                <div id="canvas-holder" style="width:40%">
+                    <canvas id="chart-area"></canvas>
+                </div>
+    """)
+
+def generate_footer(output):
+    output.write("""
+            </body>
+        </html>
+    """)
+
+def generate_pie_chart(output, activities):
+    counts = collections.defaultdict(int)
+    for timestamp, cpu, app_cpu, x, y, w, h, pixel, location, pid, ppid, app_name, window_title in get_activities():
+        counts[(pixel, app_name, window_title)] += 1
+    html_template = """
+        <script>
+        var colors = [
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+            window.chartColors.red, window.chartColors.orange, window.chartColors.yellow, window.chartColors.green, window.chartColors.blue,
+        ];
+		var config = {
+			type: 'pie',
+			data: {
+				datasets: [{
+					data: %(data)s,
+					backgroundColor: colors,
+					label: 'Time Spent Well'
+				}],
+				labels: %(labels)s,
+			},
+			options: {
+				responsive: true
+			}
+		};
+
+		window.onload = function() {
+			var ctx = document.getElementById('chart-area').getContext('2d');
+			window.myPie = new Chart(ctx, config);
+		};
+        </script>
+    """
+    labels = repr([window_title.encode("utf8") for pixel, app_name, window_title in counts.keys()])
+    data = repr(list(counts.values()))
+    html = html_template % {"data": data, "labels": labels}
+    output.write(html)
+
+def generate_full_table(output, activities):
+    output.write("<h1>All Events</h1><table border=1 width=\"1400px\">")
+    output.write("""
+        <tr>
+            <th>When</th>
+            <th>CPU</th>
+            <th>APP CPU</th>
+            <th>Dimensions</th>
+            <th>Pixel</th>
+            <th>PID</th>
+            <th>Parent PID</th>
+            <th>Location</th>
+            <th>APP NAME</th>
+            <th>Window/Tab Title</td>
+        </tr>""")
+
+    def pixelToColor(pixel):
+        return "rgba(%d,%d,%d,%d)" % eval(pixel) if pixel else "white"
+
+    for timestamp, cpu, app_cpu, x, y, w, h, pixel, location, pid, ppid, app_name, window_title in get_activities():
+        output.write("""
+        <tr>
+        <td>%s</td>
+        <td>%d%%</td>
+        <td>%d%%</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        </tr>
+        """ % (
+            datetime.datetime.fromtimestamp(timestamp),
+            int(cpu * 100),
+            int(app_cpu * 100),
+            (x, y, w, h),
+            '<div style="width:40px; height:40px; background:%s"/>' % pixelToColor(pixel),
+            pid,
+            ppid,
+            location,
+            app_name.encode('ascii','ignore'),
+            window_title.encode('ascii','ignore'),
+        ))
+    output.write("</table>")
+
 
 sqlite3.connect(get_activity_path()).cursor().execute(INIT_QUERY)

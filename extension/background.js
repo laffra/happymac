509f29a125d30051aee7e54d82c783c4479eb033
Chris Laffra <laffra@uber.com>
2018-11-13 09:12:09 +0100

    Capture email to disambiguate browser sessions

--- extension/background.js
+++ extension/background.js
@@ -1,20 +1,25 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  updateTab(tab);
+  chrome.identity.getProfileUserInfo(function (userInfo) {
+    updateTab(userInfo.email, tab);
+  });
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
   chrome.tabs.get(activeInfo.tabId, function (tab) {
-    updateTab(tab);
+    chrome.identity.getProfileUserInfo(function(userInfo) {
+      updateTab(userInfo.email, tab);
+     });
   });
 });
 
-function updateTab(tab) {
+function updateTab(email, tab) {
   var port_number = 1187;
   while (port_number < 1287) {
     try {
       var url = "http://localhost:" + port_number + "/happymac?" +
         "url=" + encodeURIComponent(tab.url) + "&" +
         "fav=" + encodeURIComponent(tab.favIconUrl) + "&" +
+        "email=" + encodeURIComponent(email) + "&" +
         "title=" + encodeURIComponent(tab.title);
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url);
